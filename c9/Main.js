2c1b6c0982ce0a6e3c3d9ef93e2aba2c621dee7f
MarioPet <mario.petkovski@MAR-PET-MAC.local>
2026-02-17 21:39:35 +0100

    Classes and Canvas

---
+++ c9/Main.js
@@ -0,0 +1,4 @@
+import { Product } from "./Product.js";
+
+const laptop = new Product("Laptop", 1200, 5);
+// console.log(laptop);
---
+++ c9/Product.js
@@ -0,0 +1,7 @@
+export class Product {
+    constructor(name, price, stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+}
